44037cce00f640d92d0c95985769fd1abd0fdc3b
jeremy776 <jeremykusuma4@gmail.com>
2021-06-29 02:42:10 +0700

    testinf

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ async function getBot(id) {
   if(!id) throw new Error("Parameter id must be filled");
   if(isNaN(id)) throw new Error("ID must be a number");
   if(id.length !== 18) throw new Error("Invalid Discord ID");
-  
   let data = await axios.get(baseURL + "/bot?id="+id);
   return data.data;
 }
