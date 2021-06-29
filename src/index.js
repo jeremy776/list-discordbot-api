3e46e842f38fa5b3ebb349e4e76c7f302f883b36
カグチ <59391215+KagChi@users.noreply.github.com>
2021-06-29 14:52:08 +0700

    feat: add typings

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const baseURL = "https://api.list-discordbot.ga";
+
+
+/**
+ * @param id {string}
+ * @returns {Promise<object>}
+ */
+
+async function getBot(id) {
+    if(!id) throw new Error('Parameters must be filled');
+    if(id.length !== 18) throw new Error('Invalid ID Discord');
+    try {
+        let get = await axios.get(baseURL + '/bot?id=' + id);
+        return get.data;
+    } catch (e) {
+        throw new Error('An error occured when fetching data, make sure if bot exist.')
+    }
+}
+
+module.exports = { getBot };
